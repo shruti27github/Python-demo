caed6a32c4d611e52542e1914d574da34f59d990
shruti27github <shruti27mala@gmail.com>
2025-11-19 20:46:54 +0530

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,10 @@
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+add=n1+n2
+print("Addition:", add)
+sub=n1-n2
+print("Subtraction:", sub)
+mul=n1*n2
+print("Multiplication:", mul)
+div=n2/n1
+print("Division:", round(div, 2))
---
+++ Task2.py
@@ -0,0 +1,3 @@
+n1=str(input("enter the first name:"))
+n2=str(input("enter the last name:"))
+print("hello,", n1,n2,"! Welcome to Python program")
